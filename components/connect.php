1776f04dfffad965492dddfd409a0740d552539c
ripon4521 <119059817+ripon4521@users.noreply.github.com>
2023-03-15 15:13:11 +0600

    Add files via upload

---
+++ components/connect.php
@@ -0,0 +1,9 @@
+<?php
+
+$db_name = 'mysql:host=sql202.epizy.com; epiz_33802136_shouppreation,epiz_33802136,Qxq0FPb3Ziau4e';
+$user_name = 'root';
+$user_password = '';
+
+$conn = new PDO($db_name, $user_name, $user_password);
+
+?>
